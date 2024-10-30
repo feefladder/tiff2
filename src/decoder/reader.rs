a8359fe67413b116ae88cca748f92ba6f62337fa
Fee Fladder <freakyfee@proton.me>
2024-10-30 04:54:52 +0100

    added flate2, made deps independent of superminor versions

--- src/decoder/reader.rs
+++ src/decoder/reader.rs
@@ -1,18 +1,40 @@
-use std::io::{self, Read};
-
-use crate::ByteOrder;
+use std::{collections::BTreeMap, io::{self, Read}, ops::Range};
+use bytes::Bytes;
+use crate::{error::TiffResult, structs::{Offset, TagData, Tag}, util::fix_endianness, ByteOrder};
 
 use async_trait::async_trait;
 
-/// Trait for a CogReader to implement. In fact these are all the same, but caching can be optimized based on which part of the tiff we're reading in.
+/// Trait for a CogReader to implement.
+/// 
+/// In fact these functions can be all the same, but caching can be optimized based on which part of the tiff we're reading in.
 #[async_trait]
 pub trait CogReader {
     // https://blog.rust-lang.org/2023/12/21/async-fn-rpit-in-traits.html#where-the-gaps-lie
     async fn read_ifd(&self, byte_start: u64, n_bytes: u64) -> Vec<u8>;
     async fn read_tag_data(&self, byte_start: u64, n_bytes: u64) -> Vec<u8>;
     async fn read_image_data(&self, byte_start: u64, n_bytes: u64) -> Vec<u8>;
+    async fn get_ranges(&self, ranges: Vec<Range<u64>>) -> Vec<Bytes>;
+    async fn get_tags(&self, tags: BTreeMap<Tag, Offset>, byte_order: ByteOrder) -> TiffResult<BTreeMap<Tag, TagData>> {
+        let mut ranges = Vec::new();
+        for (_, offset) in &tags {
+            ranges.push(offset.offset..offset.offset + offset.count * u64::try_from(offset.tag_type.size())?);
+        }
+        let resp = self.get_ranges(ranges).await;
+        let mut res = BTreeMap::new();
+        // BTreeMap keeps order, so we can safely iterate over that again
+        for (i, (tag, offset)) in tags.iter().enumerate() {
+            let mut e = TagData::new(offset.tag_type, usize::try_from(offset.count)?);
+            e.buf_mut().copy_from_slice(&resp[i][..]);
+            fix_endianness(e.buf_mut(), byte_order, offset.tag_type.primitive_size() * 8);
+            res.insert(*tag, e);
+        }
+        println!("{tags:?}");
+        Ok(res)
+    }
 }
 
+/// Reader that is aware of the byte order  
+/// TODO: **deprecate** in favour of chunk-based approach in `Entry` and `Ifd`
 pub struct EndianReader<R> {
     pub(super) reader: R,
     pub byte_order: ByteOrder,
